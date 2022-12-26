8d5b907407a296d634cb34a7042e146d1910e065
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-26 23:02:30 +0000

    Fix instance attributes for Graph

--- boruvka.py
+++ boruvka.py
@@ -5,24 +5,20 @@ from typing import List
 
 
 class Graph:
-    def __init__(self, nodes: set = set(), edges: List[tuple] = []):
-        self.nodes = nodes
+    def __init__(self, num_nodes: int):
+        self.num_nodes = num_nodes
+        self.nodes = [node for node in range(1, num_nodes + 1)]
         # [(node1, node2, weight)]
-        self.edges = edges
-        self.components = {}
+        self.edges = []
+        # {node: parent} to represent the component trees. Initially, each node
+        # is its own parent, as the graph is disconnected.
+        self.components = {node: node for node in self.nodes}
 
-    def get_nodes(self) -> set:
+    def get_nodes(self) -> list:
         return self.nodes
 
     def get_edges(self) -> List[tuple]:
         return self.edges
-
-    def add_node(self, node: str) -> bool:
-        if node in self.nodes:
-            raise ValueError(f"Node {node} already exists")
-
-        self.nodes.add(node)
-        return True
 
     def add_edge(self, node1: str, node2: str, weight: int) -> bool:
         if node1 not in self.nodes:
@@ -39,27 +35,7 @@ class Graph:
 
 def main():
     # https://www.statisticshowto.com/boruvkas-algorithm/
-    graph1 = Graph()
-    for node in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]:
-        graph1.add_node(node)
-    graph1.add_edge("A", "B", 1)
-    graph1.add_edge("A", "E", 7)
-    graph1.add_edge("B", "C", 2)
-    graph1.add_edge("B", "F", 8)
-    graph1.add_edge("B", "G", 9)
-    graph1.add_edge("C", "D", 3)
-    graph1.add_edge("C", "H", 1)
-    graph1.add_edge("D", "H", 2)
-    graph1.add_edge("E", "F", 4)
-    graph1.add_edge("E", "I", 4)
-    graph1.add_edge("E", "J", 8)
-    graph1.add_edge("F", "G", 5)
-    graph1.add_edge("F", "J", 3)
-    graph1.add_edge("F", "K", 4)
-    graph1.add_edge("G", "H", 6)
-    graph1.add_edge("G", "K", 9)
-    graph1.add_edge("H", "L", 5)
-    graph1.add_edge("K", "L", 6)
+    graph1 = Graph(9)
     graph1.find_mst_with_boruvka()
 
 
