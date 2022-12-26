eccd2f6e7c32e2c44c813b742a483b1b2ceb5e56
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-26 23:03:24 +0000

    Add example graph

--- boruvka.py
+++ boruvka.py
@@ -20,7 +20,7 @@ class Graph:
     def get_edges(self) -> List[tuple]:
         return self.edges
 
-    def add_edge(self, node1: str, node2: str, weight: int) -> bool:
+    def add_edge(self, node1: int, node2: int, weight: int) -> bool:
         if node1 not in self.nodes:
             raise ValueError(f"Node {node1} does not exist")
         if node2 not in self.nodes:
@@ -34,8 +34,22 @@ class Graph:
 
 
 def main():
-    # https://www.statisticshowto.com/boruvkas-algorithm/
     graph1 = Graph(9)
+    graph1.add_edge(0, 1, 4)
+    graph1.add_edge(0, 6, 7)
+    graph1.add_edge(1, 6, 11)
+    graph1.add_edge(1, 7, 20)
+    graph1.add_edge(1, 2, 9)
+    graph1.add_edge(2, 3, 6)
+    graph1.add_edge(2, 4, 2)
+    graph1.add_edge(3, 4, 10)
+    graph1.add_edge(3, 5, 5)
+    graph1.add_edge(4, 5, 15)
+    graph1.add_edge(4, 7, 1)
+    graph1.add_edge(4, 8, 5)
+    graph1.add_edge(5, 8, 12)
+    graph1.add_edge(6, 7, 1)
+    graph1.add_edge(7, 8, 3)
     graph1.find_mst_with_boruvka()
 
 
